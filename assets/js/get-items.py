f396b2cb9b5fd0b6a9db53616da3352c2612e5cc
Trace Pillars <trace.pillars@e7solutions.com>
2014-04-11 22:29:56 -0400

    items

--- assets/js/get-items.py
+++ assets/js/get-items.py
@@ -2,7 +2,7 @@ from urllib.request import urlopen
 from json import loads, dumps
 from os import system
 
-with open("items.txt") as i:
+with open("item.json") as i:
     items_json = loads(i.read())
 
 print(items_json)
